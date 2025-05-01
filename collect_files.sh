4d749aa6ba2edd7a2edff356b20208ef9379f39d
yapetry <yana.petrosyan.205@mail.ru>
2025-05-01 11:24:20 +0300

    add solution

--- collect_files.sh
+++ collect_files.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+
 from="$1"
 to="$2"
-find "$from" -type f | while read file; do
-    # https://www.geeksforgeeks.org/basename-command-in-linux-with-examples/
-    curName=$(basename "$file")
-    # https://stackoverflow.com/questions/15529779/unix-mv-backup-numbered
-    cp --backup=numbered "$file" "$to/$curName"
-done
+flag=false
+
+if [ "$1" = "--max_depth" ]; then
+    from="$3"
+    to="$4"
+    depth="$2"
+    flag=true
+fi
+
+if [ "$flag" = false ]; then
+    find "$from" -type f | while read file; do
+        # https://www.geeksforgeeks.org/basename-command-in-linux-with-examples/
+        curName=$(basename "$file")
+        # https://stackoverflow.com/questions/15529779/unix-mv-backup-numbered
+        cp --backup=numbered "$file" "$to/$curName"
+    done
+else
+    # https://www.geeksforgeeks.org/mindepth-maxdepth-linux-find-command-limiting-search-specific-directory/
+    find "$from" -maxdepth "$depth" -type f | while read file; do
+        # https://www.geeksforgeeks.org/basename-command-in-linux-with-examples/
+        curName=$(basename "$file")
+        # https://stackoverflow.com/questions/15529779/unix-mv-backup-numbered
+        cp --backup=numbered "$file" "$to/$curName"
+    done
+fi
